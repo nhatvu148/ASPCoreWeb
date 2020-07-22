be2cabcd2b9982658a4c7cb0d5940827b40039a9
nhatvu148 <nhatvu148@gmail.com>
2020-07-23 00:25:04 +0900

    initial commit

---
+++ ASPCoreWeb/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPCoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWeb.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movies/Random
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!", Id = 123456 };
+            //return View(movie);
+            //return Content("Hello World!");
+            //return NotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+    }
+}
